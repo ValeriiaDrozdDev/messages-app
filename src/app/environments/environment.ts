fecb05b917aa9e918f27b58dd86c0add353ddd4d
ValeriiaDrozdDev <valeriia.drozd.dev@gmail.com>
2023-01-12 15:11:50 +0200

    Project is done

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAxVVeONaxeIFLA-b-QtXabGK6FgaRQImw',
+    authDomain: 'messages-app-1636f.firebaseapp.com',
+    projectId: 'messages-app-1636f',
+    storageBucket: 'messages-app-1636f.appspot.com',
+    messagingSenderId: '784613311248',
+    appId: '1:784613311248:web:b272522433f9f400061b95',
+  },
+};
